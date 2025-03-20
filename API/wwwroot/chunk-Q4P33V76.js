4688afb1128c2fec129eda615a22f91c24390f0c
Jason <jason.brigou@hotmail.com>
2025-03-20 17:45:42 +0100

    End of course

---
+++ API/wwwroot/chunk-Q4P33V76.js
@@ -0,0 +1,1 @@
+import{b as u}from"./chunk-YEQ56EBD.js";import{j as o}from"./chunk-GQK6K4ZZ.js";import{ka as t,r as e,x as n}from"./chunk-55L6EDGV.js";var A=(m,a)=>{let r=t(u),c=t(o);return r.currentUser()?e(!0):r.getAuthState().pipe(n(i=>i.isAuthenticated?!0:(c.navigate(["/account/login"],{queryParams:{returnUrl:a.url}}),!1)))};export{A as a};
